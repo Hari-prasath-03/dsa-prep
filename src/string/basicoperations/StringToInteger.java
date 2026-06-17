f95767ec616c7157a7ef82c5be53ed4fde155430
Hari-prasath-03 <hariprasathk2023@gmail.com>
2026-06-17 22:06:42 +0530

    basic string

---
+++ src/string/basicoperations/StringToInteger.java
@@ -0,0 +1,27 @@
+package string.basicoperations;
+
+public class StringToInteger {
+    public static int myAtoi(String s) {
+        int i = 0, res = 0, sign = 1, n = s.length();
+        if (n == 0) return 0;
+
+        while (i < n && s.charAt(i) == ' ') i++;
+
+        if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) sign = s.charAt(i++) == '-' ? -1 : 1;
+
+        while (i < n && s.charAt(i) >= '0' && s.charAt(i) <= '9') {
+            if (res > Integer.MAX_VALUE / 10 || res == Integer.MAX_VALUE / 10 && s.charAt(i) - '0' > Integer.MAX_VALUE % 10)
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            res = res * 10 + (s.charAt(i++) - '0');
+        }
+
+        return res * sign;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(myAtoi("42"));
+        System.out.println(myAtoi(" -042"));
+        System.out.println(myAtoi("1337c0d3"));
+        System.out.println(myAtoi("0-1"));
+    }
+}
---
+++ src/string/twopointers/StringCompression.java
@@ -0,0 +1,28 @@
+package string.twopointers;
+
+public class StringCompression {
+    public static int compress(char[] chars) {
+        int write = 0, index = 0;
+
+        for (int i = 0; i < chars.length; i++) {
+            write++;
+            if (i + 1 == chars.length || chars[i] != chars[i + 1]) {
+                chars[index++] = chars[i];
+                if (write != 1) {
+                    for (char ch: Integer.toString(write).toCharArray()) {
+                        chars[index++] = ch;
+                    }
+                }
+                write = 0;
+            }
+        }
+
+        return index;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(compress(new char[] {'a', 'a', 'b', 'b', 'c', 'c', 'c'}));
+        System.out.println(compress(new char[] {'a'}));
+        System.out.println(compress(new char[] {'a','b','b','b','b','b','b','b','b','b','b','b','b'}));
+    }
+}
